0e2f07db78952bd319f1a1656bc527b0e7a170d2
Sailesh Gurung <saileshgurung98@gmail.com>
2025-03-18 20:16:28 +0545

    initial

---
+++ src/App1.vue
@@ -0,0 +1,41 @@
+<script>
+export default {
+  data() {
+    return {
+      name: "Sailesh Gurung",
+      status: "pending",
+      tasks: ["Task One", "Task Two", "Task Three"],
+      link: "https://google.com",
+    };
+  },
+  methods: {
+    toggleStatus() {
+      if (this.status === "active") {
+        this.status = "pending";
+      } else if (this.status === "pending") {
+        this.status = "inactive";
+      } else {
+        this.status = "active";
+      }
+    },
+  },
+};
+</script>
+
+<template>
+  <h1>{{ name }}</h1>
+  <p v-if="status === 'active'">User is active</p>
+  <p v-else-if="status === 'pending'">User is Pending</p>
+  <p v-else>User is inactive</p>
+
+  <h3>Tasks:</h3>
+  <ul>
+    <li v-for="task in tasks" :key="task">{{ task }}</li>
+  </ul>
+  <!-- <a v-bind:href="link">Click For Google</a> -->
+  <a :href="link">Click For Google</a>
+  <!-- <button v-on:click="toggleStatus">Change Status</button> -->
+  <button @click="toggleStatus">Change Status</button>
+</template>
+
+<style scoped></style>
